5a3ba23ff5428aa745bf363814ec9162e9b8cb56
Jericho-NSK <panchenkodv@ya.ru>
2023-01-06 01:06:35 +0700

    Adding Kitty file

---
+++ Kitty.py
@@ -0,0 +1,6 @@
+from simple_colors import *
+
+print(red('\t\t\t\t\t\t\t/\_/\\\n\t\t\t\t\t\t\t>^,^<'), yellow(' ___ PURR PURR'), cyan('\n\t\t\t\t\t\t\t / \\\n\t\t\t\t\t\t\t(|_|)_/\t\t'),
+      green('/***\\', 'underlined'))
+
+print('\t\t\t\t\t\t\t/\_/\\\n\t\t\t\t\t\t\t>^,^< ___ MEOW!!!''\n\t\t\t\t\t\t\t / \\\n\t\t\t\t\t\t\t(|_|)_/\t\t''/___\\')

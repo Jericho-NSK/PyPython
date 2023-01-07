d7c62cd4b6e49a9076440462a1ef487d5ca99d4a
Jericho-NSK <panchenkodv@ya.ru>
2023-01-07 17:30:23 +0700

    Adding Match Case examples.py

--- Lesson.py
+++ Lesson.py
@@ -1,1 +1,15 @@
-print('hi')
+class Qwe:
+    def __str__(self):
+        return 'text Qwe'
+    # pass
+
+
+# def f(cls):
+#     print(cls.color)
+
+Point2 = type('Point', (Qwe,), {'color': 'red'})
+Point = type('Point', (Qwe,), {'color': 'red', '__repr__': lambda self: 'text'})
+p = Point()
+# print(p.f())
+print(Point.__name__)
+print(Qwe())

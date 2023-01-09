1769dbee6fa8bc08b8ebef7c4f75cd8985033d57
Jericho-NSK <panchenkodv@ya.ru>
2023-01-10 00:45:00 +0700

    Deleting last lesson

--- Lesson.py
+++ Lesson.py
@@ -1,15 +1,1 @@
-class Qwe:
-    def __str__(self):
-        return 'text Qwe'
-    # pass
-
-
-# def f(cls):
-#     print(cls.color)
-
-Point2 = type('Point', (Qwe,), {'color': 'red'})
-Point = type('Point', (Qwe,), {'color': 'red', '__repr__': lambda self: 'text'})
-p = Point()
-# print(p.f())
-print(Point.__name__)
-print(Qwe())
+import this

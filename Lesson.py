9a48de5cd4fbb254a4a33151d06544a05596f0fc
Jericho-NSK <panchenkodv@ya.ru>
2023-01-28 01:28:14 +0700

    Add date to names of packages

--- Lesson.py
+++ Lesson.py
@@ -1,8 +1,20 @@
-a = [2, 5]
-def f(a):
+import tkinter as tk
 
-    a+=4,
-    print(a)
+class Example(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        self.l1 = tk.Radiobutton(self, text="Hover over me")
+        self.l1.pack(side="top")
+        self.l1.bind("<Enter>", self.on_enter)
+        self.l1.bind("<Leave>", self.on_leave)
 
-f(a)
-f(a)
+    def on_enter(self, event):
+        self.l1.configure(background='red')
+
+    def on_leave(self, enter):
+        self.l1.configure(background="white")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    Example(root).pack(side="top", fill="both", expand="true")
+    root.mainloop()

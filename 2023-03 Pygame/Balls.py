a44394b2b2c8a1d4e99aac7bb7f4748a193cd164
Jericho-NSK <panchenkodv@ya.ru>
2023-02-06 01:01:18 +0700

    Update

--- 2023-03 Pygame/Balls.py
+++ 2023-03 Pygame/Balls.py
@@ -2,11 +2,15 @@ from pygame import sprite
 
 
 class Ball(sprite.Sprite):
-    def __init__(self, x, speed, surf, group):
-        super().__init__(self)
+    def __init__(self, x, speed, surf, group, size):
+        super().__init__()
         self.image = surf
-        self.rect = self.image.get_rect(center=(x, 0))
+        self.rect = self.image.get_rect(center=(x, -size))
         self.speed = speed
         self.add(group)
 
-    def update(self, *args: Any, **kwargs: Any) -> None:
+    def update(self, height, size) -> None:
+        if self.rect.y < height - size:
+            self.rect.y += self.speed
+        else:
+            self.kill()
--- 2023-03 Pygame/Game.py
+++ 2023-03 Pygame/Game.py
@@ -1,24 +1,47 @@
+from random import randint
+
 from pygame import *
+
 from Balls import Ball
 
 init()
+time.set_timer(USEREVENT, 1000)
 
-W, H = 600, 400
+W, H = 800, 600
+ball_size = W / 10
 FPS = 24
 window = display.set_mode((W, H))
 display.set_caption("Pet Game")
-display.set_icon(image.load('Back.png'))
+bg = image.load('Back.png').convert_alpha()
+bg = transform.scale(bg, (W, H))
 
-images = image.load('boom.png').convert_alpha(), image.load('FirstBoom.png').convert_alpha()
 
 balls = sprite.Group()
-balls.add()
+
+images_no_scale = 'Bomb_1.png', 'Bomb_2.png', 'Bomb_3.png'
+balls_surf = []
+for img in images_no_scale:
+    img = image.load(img).convert_alpha()
+    img = transform.scale(img, (ball_size, ball_size))
+    balls_surf.append(img)
+
+
+def create_ball(group, size=ball_size):
+    index = randint(0, len(balls_surf) - 1)
+    x = randint(20, W - 20)
+    speed = randint(1, 4)
+    return Ball(x, speed, balls_surf[index], group, size)
+
 
 while True:
-    for event in event.get():
-        if event.type == QUIT:
+    for e in event.get():
+        if e.type == QUIT:
             exit()
+        elif e.type == USEREVENT:
+            create_ball(balls)
 
+    window.blit(bg, (0, 0))
     balls.draw(window)
-
+    display.update()
     time.Clock().tick(FPS)
+    balls.update(H, ball_size)

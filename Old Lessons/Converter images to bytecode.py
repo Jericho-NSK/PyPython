4fe9ecf5444aab4222fed5b78a4f19878d1f6c22
Jericho-NSK <panchenkodv@ya.ru>
2023-02-07 03:40:36 +0700

    Add resize by reduce option

--- Old Lessons/Converter images to bytecode.py
+++ Old Lessons/Converter images to bytecode.py
@@ -3,7 +3,8 @@ from tkinter import PhotoImage
 
 from PIL import Image
 
-icon = Image.open(r'G:\Python\PyPython\2023-03 Pygame\Bomb_3.png', mode='r').resize((80, 80)).crop()  # требуемый размер
+icon = Image.open(r'G:\Python\PyPython\2023-03 Pygame\cart.png', mode='r').resize((100, 94)).crop()  # требуемый размер
+# icon = Image.open(r'G:\Python\PyPython\2023-03 Pygame\cart.png', mode='r').reduce(5).crop()  # Уменьшение в 10 раз
 icon_arr = BytesIO()
 icon.save(icon_arr, format='PNG')
 icon_arr = icon_arr.getvalue()
@@ -14,5 +15,5 @@ byte_icon = b'...'  # <- тут длинная строка в байтовом 
 
 # final_img = PhotoImage(data=byte_icon, format='png')
 
-with open('new_file3.png', 'wb') as file:  # Сохранение в новый файл
+with open('../2023-03 Pygame/new_cart.png', 'wb') as file:  # Сохранение в новый файл
     file.write(icon_arr)

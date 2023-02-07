e53cc715ce6ac7f84258c9bd17d2d8b90c8f3342
Jericho-NSK <panchenkodv@ya.ru>
2023-02-08 04:36:09 +0700

    Convert image for new project

--- Old Lessons/Converter images to bytecode.py
+++ Old Lessons/Converter images to bytecode.py
@@ -3,17 +3,17 @@ from tkinter import PhotoImage
 
 from PIL import Image
 
-icon = Image.open(r'G:\Python\PyPython\2023-03 Pygame\cart.png', mode='r').resize((100, 94)).crop()  # требуемый размер
+icon = Image.open(r'G:\Python\PyPython\2023-02.2 Flappy Pygame Pet Project\images\chair.png', mode='r').resize((100, 100)).crop()  # требуемый размер
 # icon = Image.open(r'G:\Python\PyPython\2023-03 Pygame\cart.png', mode='r').reduce(5).crop()  # Уменьшение в 10 раз
 icon_arr = BytesIO()
 icon.save(icon_arr, format='PNG')
 icon_arr = icon_arr.getvalue()
-print(icon_arr)  # <- скопировал результат из консоли в новую переменную byte_icon
+# print(icon_arr)  # <- скопировал результат из консоли в новую переменную byte_icon
 
 byte_icon = b'...'  # <- тут длинная строка в байтовом виде. Весь код выше можно удалять.
 # Кроме "from tkinter import PhotoImage", так как он еще нужен ниже
 
 # final_img = PhotoImage(data=byte_icon, format='png')
 
-with open('../2023-03 Pygame/new_cart.png', 'wb') as file:  # Сохранение в новый файл
+with open(r'G:\Python\PyPython\2023-02.2 Flappy Pygame Pet Project\images\new_chair.png', 'wb') as file:  # Сохранение в новый файл
     file.write(icon_arr)

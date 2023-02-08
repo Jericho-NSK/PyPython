08638ea92f0691c55dfe47f65a5d044e86a53075
Jericho-NSK <panchenkodv@ya.ru>
2023-02-08 10:01:51 +0700

    Change filename

--- Old Lessons/Converter images to bytecode.py
+++ Old Lessons/Converter images to bytecode.py
@@ -3,7 +3,8 @@ from tkinter import PhotoImage
 
 from PIL import Image
 
-icon = Image.open(r'G:\Python\PyPython\2023-02.2 Flappy Pygame Pet Project\images\chair.png', mode='r').resize((100, 100)).crop()  # требуемый размер
+icon = Image.open(r'G:\Python\PyPython\2023-02.2 Flappy Pygame Pet Project\images\original_column.png', mode='r').resize((100, 133)).crop()  #
+# требуемый размер
 # icon = Image.open(r'G:\Python\PyPython\2023-03 Pygame\cart.png', mode='r').reduce(5).crop()  # Уменьшение в 10 раз
 icon_arr = BytesIO()
 icon.save(icon_arr, format='PNG')
@@ -15,5 +16,5 @@ byte_icon = b'...'  # <- тут длинная строка в байтовом 
 
 # final_img = PhotoImage(data=byte_icon, format='png')
 
-with open(r'G:\Python\PyPython\2023-02.2 Flappy Pygame Pet Project\images\new_chair.png', 'wb') as file:  # Сохранение в новый файл
+with open(r'G:\Python\PyPython\2023-02.2 Flappy Pygame Pet Project\images\column.png', 'wb') as file:  # Сохранение в новый файл
     file.write(icon_arr)

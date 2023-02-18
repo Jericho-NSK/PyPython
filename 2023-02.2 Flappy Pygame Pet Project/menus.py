127499b48c63a788d64753ec52858b01517679f1
Jericho-NSK <panchenkodv@ya.ru>
2023-02-19 00:09:37 +0700

    add About menu

--- 2023-02.2 Flappy Pygame Pet Project/menus.py
+++ 2023-02.2 Flappy Pygame Pet Project/menus.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame_menu
+from webbrowser import open_new_tab
 
 from constants import HEIGHT, WIDTH, FPS
 
@@ -7,7 +8,8 @@ from constants import HEIGHT, WIDTH, FPS
 class Menus:
     theme = pygame_menu.themes.THEME_BLUE.copy()
     theme.set_background_color_opacity(0.5)
-    theme.widget_font_size = 50
+    theme.widget_font_size = 40
+    theme.widget_font_color = 'blue'
     theme.widget_selection_color = 'purple'
     theme.widget_padding = 10
 
@@ -23,21 +25,37 @@ class Menus:
                                      theme=theme,
                                      )
 
+    about_menu = pygame_menu.Menu(title='About',
+                                     width=2 * WIDTH // 3,
+                                     height=2 * HEIGHT // 3,
+                                     theme=theme,
+                                     )
+
     def __init__(self, game):
         self.surface = game.main_window.window
         self.main_menu.add.button('Play', game.mainloop)
         self.main_menu.add.button('Settings', self.settings_menu)
+        self.main_menu.add.button('About', self.about_menu)
         self.main_menu.add.button('Exit', pygame_menu.events.EXIT)
 
         self.settings_menu.add.toggle_switch('Full screen',
                                              default=pygame.display.is_fullscreen(),
                                              onchange=lambda x: pygame.display.toggle_fullscreen(),
                                              )
+        self.settings_menu.add.toggle_switch('Music',
+                                             default=pygame.display.is_fullscreen(),
+                                             onchange=lambda x: pygame.display.toggle_fullscreen(),
+                                             )
+        self.settings_menu.add.toggle_switch('Sounds',
+                                             default=pygame.display.is_fullscreen(),
+                                             onchange=lambda x: pygame.display.toggle_fullscreen(),
+                                             )
         self.settings_menu.add.button('Back', pygame_menu.events.BACK)
 
-
-
-
+        self.about_menu.add.label('NOT a flappy bird v1.0\nCreated by Jericho for Pet-project', font_color=(0, 135, 0))
+        self.about_menu.add.vertical_margin(30)
+        self.about_menu.add.button('Link to GitHub', lambda: open_new_tab('https://github.com/Jericho-NSK/PyPython/tree/main/2023-02.2%20Flappy%20Pygame%20Pet%20Project'))
+        self.about_menu.add.button('Back', pygame_menu.events.BACK)
 
     # theme = pygame_menu.themes.THEME_BLUE.copy()
     # theme.set_background_color_opacity(0.5)

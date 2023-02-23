274eccad8500269e1db09c02de7182658f6cdc70
Jericho-NSK <panchenkodv@ya.ru>
2023-02-23 18:24:41 +0700

    add ESC pressing event

--- 2023-02.2 Flappy Pygame Pet Project/main_game.py
+++ 2023-02.2 Flappy Pygame Pet Project/main_game.py
@@ -1,5 +1,5 @@
-from inspect  import getouterframes, currentframe
-from  sys import exit, getrecursionlimit, setrecursionlimit
+from inspect import getouterframes, currentframe
+from sys import exit, getrecursionlimit, setrecursionlimit
 
 import pygame
 
@@ -17,6 +17,7 @@ class Game:
     score = 0
 
     def __init__(self):
+        self.breakdown = False
         self.game_starts = False
         self.track = START_TRACK
         self.main_window = Window(self)
@@ -26,14 +27,16 @@ class Game:
 
     def crash(self):
         for wall in self.walls:
-            if wall.rect.collidepoint(self.bird.rect.midright) or wall.rect.collidepoint(self.bird.rect.bottomright):
+            if wall.rect.collidepoint(self.bird.rect.midright) or wall.rect.collidepoint(self.bird.rect.bottomright) or self.breakdown:
                 self.game_starts = False
                 self.track = START_TRACK
                 self.bird.timer = 0
                 self.bird.jump = 0
                 self.bird.image = Images.bird_images[-1]
+                self.main_window.elements.escape_timer = 0
                 self.lives -= 1
                 self.menu.crash_menu.enable()
+                break
             # self.sound_catch.play()
 
     def new_game(self):
@@ -41,9 +44,12 @@ class Game:
         self.score = 0
         Wall.speed = SPEED
         self.main_window = Window(self)
-        return self.resume_game()
+        self.resume_game()
 
     def resume_game(self):
+        self.breakdown = False
+        self.menu.disable()
+        self.game_starts = True
         self.bird.rect.center = BIRD_START
         self.bird.image = Images.bird_images[0]
         for wall in self.walls:
@@ -51,9 +57,8 @@ class Game:
         self.mainloop()
 
     def mainloop(self):
-        self.menu.disable()
-        self.game_starts = True
         pygame.time.set_timer(pygame.USEREVENT, 3000 // Wall.speed)
+
         if len(getouterframes(currentframe())) > getrecursionlimit() - 100:
             setrecursionlimit(getrecursionlimit() + 100)
 
@@ -65,8 +70,10 @@ class Game:
                 elif event.type == pygame.QUIT:
                     exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        self.main_window.escape_timer = 3 * FPS
+                    if event.key == pygame.K_ESCAPE and not self.main_window.elements.escape_timer:
+                        self.main_window.elements.escape_timer = 3 * FPS
+                    elif event.key == pygame.K_ESCAPE:
+                        self.breakdown = True
                     elif event.key == pygame.K_SPACE:
                         self.bird.jump = 35
                     elif event.key == pygame.K_w or event.key == pygame.K_UP:
@@ -81,4 +88,5 @@ class Game:
             self.main_window.update_window(self)
 
 
-Game()
+if __name__ == '__main__':
+    Game()

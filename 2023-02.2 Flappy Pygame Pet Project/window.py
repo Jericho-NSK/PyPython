da87a50bf6d2c00d5f4183ff31a7f29ca90acdef
Jericho-NSK <panchenkodv@ya.ru>
2023-02-23 18:24:35 +0700

    replace elements to own class

--- 2023-02.2 Flappy Pygame Pet Project/window.py
+++ 2023-02.2 Flappy Pygame Pet Project/window.py
@@ -1,20 +1,16 @@
 import pygame
 
-from constants import WIDTH, FPS, WINDOW, FONT, HEIGHT
+from constants import WIDTH, FPS, WINDOW, HEIGHT
+from elements import Elements
 from images_and_sounds import Images
-from walls import Wall
 
 
 class Window:
     window = WINDOW
 
     def __init__(self, game):
-        self.score = FONT.render(f'SCORE: {game.score}', True, 'red')
-        self.speed = FONT.render(f'SPEED: {Wall.speed}', True, 'red')
-        self.escape = FONT.render(f'''NO PAUSES! ONLY HARDCORE!''', True, 'red')
         self.bg_rect = Images.bg.get_rect()
-        self.heart_rect = Images.heart.get_rect()
-        self.escape_timer = 0
+        self.elements = Elements(game, self)
 
     def update_window(self, game):
         self.window.blit(Images.bg, (self.bg_rect.x, 0))
@@ -25,15 +21,19 @@ class Window:
         game.walls.draw(self.window)
 
         self.window.blits((
-            (self.score, (20, 20)),
-            (self.speed, (20, 60)),
-            *((Images.heart, (WIDTH - self.heart_rect.w * (heart + 1), self.heart_rect.y)) for heart in range(game.lives)),
+            (self.elements.score, (20, 20)),
+            (self.elements.speed, (20, 60)),
+            *((Images.heart, (WIDTH - self.elements.heart_rect.w * (heart + 1),
+                              self.elements.heart_rect.y)) for heart in range(game.lives)),
             (game.bird.image, (game.bird.rect.centerx, game.bird.rect.centery)),
         ), False)
 
-        if self.escape_timer:
-            self.window.blit(self.escape, (WIDTH // 3, HEIGHT * 0.9))
-            self.escape_timer -= 1
+        if self.elements.escape_timer:
+            self.window.blit(self.elements.escape_up,
+                             (WIDTH // 2 - self.elements.escape_up_rect.w // 2, HEIGHT - 3 * self.elements.escape_up_rect.h))
+            self.window.blit(self.elements.escape_down,
+                             (WIDTH // 2 - self.elements.escape_down_rect.w // 2, HEIGHT - 2 * self.elements.escape_down_rect.h))
+            self.elements.escape_timer -= 1
         if game.bird.image != Images.bird_images[-1]:
             game.bird.flying(game)
         if game.game_starts:

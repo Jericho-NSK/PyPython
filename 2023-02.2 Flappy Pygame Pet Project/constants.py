094043c1faac1c3aff7d184c082eea3b0d154b50
Jericho-NSK <panchenkodv@ya.ru>
2023-02-24 13:48:28 +0700

    replace ESCAPE text coordinates

--- 2023-02.2 Flappy Pygame Pet Project/constants.py
+++ 2023-02.2 Flappy Pygame Pet Project/constants.py
@@ -9,7 +9,7 @@ BIRD_SIZE = 40, 30
 BIRD_START = WIDTH // 10, HEIGHT // 3
 START_LIVES = 3
 FPS = 60
-SPEED = 20
+SPEED = 2
 DIFFICULTY_MODS = (1, 10, 20, 35, 55, 75, 100, 130, 165, 200)
 FONT = pygame.font.SysFont('comicsanms', size=48, italic=True)
 WINDOW = pygame.display.set_mode(size=(WIDTH, HEIGHT),

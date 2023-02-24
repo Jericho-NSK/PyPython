306bd38290f86cd333e86c5949a1222ab716fed3
Jericho-NSK <panchenkodv@ya.ru>
2023-02-24 13:48:31 +0700

    replace ESCAPE text coordinates

--- 2023-02.2 Flappy Pygame Pet Project/elements.py
+++ 2023-02.2 Flappy Pygame Pet Project/elements.py
@@ -1,4 +1,4 @@
-from constants import FONT
+from constants import FONT, WIDTH, HEIGHT
 from images_and_sounds import Images
 from walls import Wall
 
@@ -12,7 +12,9 @@ class Elements:
         self.escape_up = FONT.render('NO PAUSES! ONLY HARDCORE!', True, 'red')
         self.escape_down = FONT.render('PRESS ESCAPE AGAIN TO GIVE UP', True, 'red')
         self.escape_up_rect = self.escape_up.get_rect()
+        self.escape_up_rect.center = WIDTH //2, HEIGHT - 3 * self.escape_up_rect.h
         self.escape_down_rect = self.escape_down.get_rect()
+        self.escape_down_rect.center = WIDTH //2, HEIGHT - 2 * self.escape_down_rect.h
         self.escape_timer = 0
 
         self.heart_rect = Images.heart.get_rect()

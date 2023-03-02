a396f9c80ddd0ce6224e2892514f2e765e1ca0e7
Jericho-NSK <panchenkodv@ya.ru>
2023-03-02 08:07:07 +0700

    Add absolute path

--- 2023-02.2_Flappy_Pygame_Pet_Project/images_and_sounds.py
+++ 2023-02.2_Flappy_Pygame_Pet_Project/images_and_sounds.py
@@ -1,16 +1,18 @@
 import pygame
+import os
 
-
+abspath = os.getcwd() + "/"
 class Images:
+
     images = 'bird_up.png', 'bird_down.png', 'bird_crash.png'
-    bird_images = [pygame.image.load('images/' + bird).convert_alpha() for bird in images]
+    bird_images = [pygame.image.load(abspath + 'images/' + bird).convert_alpha() for bird in images]
     bird_up = pygame.transform.rotate(bird_images[1], angle=15)
     bird_down = pygame.transform.rotate(bird_images[0], angle=-15)
 
-    bg = pygame.image.load('images/bg.png').convert()
+    bg = pygame.image.load(abspath + 'images/bg.png').convert()
 
     pygame.display.set_caption('NOT a flappy bird')
     pygame.display.set_icon(bird_images[-1])
 
-    wall_image = pygame.image.load('images/column.png').convert_alpha()
-    heart = pygame.image.load('images/heart.png').convert_alpha()
+    wall_image = pygame.image.load(abspath + 'images/column.png').convert_alpha()
+    heart = pygame.image.load(abspath + 'images/heart.png').convert_alpha()

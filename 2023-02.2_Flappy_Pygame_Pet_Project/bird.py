e3b2ccb50a4cda20e3f9b8d066447ddc9824ab59
Jericho-NSK <panchenkodv@ya.ru>
2023-03-05 11:12:34 +0700

    added description

--- 2023-02.2_Flappy_Pygame_Pet_Project/bird.py
+++ 2023-02.2_Flappy_Pygame_Pet_Project/bird.py
@@ -5,6 +5,7 @@ from images_and_sounds import Images
 
 
 class Bird(pygame.Surface):
+    """Class for Bird's mechanics"""
     timer = FPS // 2
 
     def __init__(self):
@@ -15,7 +16,8 @@ class Bird(pygame.Surface):
         self.jump = 0
         self.wings_up = False
 
-    def flying(self, game):
+    def jumping(self):
+        """Jumping mechanics"""
         if self.jump > 14:
             self.image = Images.bird_up
             self.rect.centery -= int((0.1 * self.jump) ** 2) - 1
@@ -27,21 +29,30 @@ class Bird(pygame.Surface):
             self.timer = 0
             self.wings_up = False
 
+    def flapping_wings(self):
+        """Changes the position of the wings"""
         if not self.timer and not self.jump:
             self.image = Images.bird_images[self.wings_up]
-            self.wings_up ^= 1
-            # self.wings_up = not self.wings_up
+            self.wings_up ^= 1  # is equal to self.wings_up = not self.wings_up
             self.timer = FPS // 3
         else:
             self.timer -= 1
 
-        if game.game_starts:
-            self.rect.centery += 1
-            if self.rect.bottom > HEIGHT - self.rect.height // 2:
-                self.rect.bottom = HEIGHT - self.rect.height // 2
-            if self.rect.top < -self.rect.height // 2:
-                self.rect.top = -self.rect.height // 2
+    def gravity(self):
+        """Gravity and limits"""
+        self.rect.centery += 1
+        if self.rect.bottom > HEIGHT - self.rect.height // 2:
+            self.rect.bottom = HEIGHT - self.rect.height // 2
+        if self.rect.top < -self.rect.height // 2:
+            self.rect.top = -self.rect.height // 2
 
+    def flying(self, game):
+        """Functions for flying and controls of Bird"""
+        self.jumping()
+        self.flapping_wings()
+
+        if game.game_starts:
+            self.gravity()
             key = pygame.key.get_pressed()
             if any((key[pygame.K_a], key[pygame.K_LEFT])) and self.rect.centerx > 0:
                 self.rect.centerx -= 4

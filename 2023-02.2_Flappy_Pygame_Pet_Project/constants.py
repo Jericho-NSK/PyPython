eee8bcc0d366e1f21c8ba8c90d7452b7ecf09dfc
Jericho-NSK <panchenkodv@ya.ru>
2023-03-07 18:39:28 +0700

    added docstrings

--- 2023-02.2_Flappy_Pygame_Pet_Project/constants.py
+++ 2023-02.2_Flappy_Pygame_Pet_Project/constants.py
@@ -1,3 +1,4 @@
+"""Global constants of the game"""
 import pygame
 
 pygame.init()
@@ -9,7 +10,7 @@ BIRD_SIZE = 40, 30
 BIRD_START = WIDTH // 10, HEIGHT // 3
 START_LIVES = 3
 FPS = 60
-SPEED = 2
+SPEED = 1
 DIFFICULTY_MODS = (1, 10, 20, 35, 55, 75, 100, 130, 165, 200)
 FONT = pygame.font.SysFont('comicsanms', size=48, italic=True)
 WINDOW = pygame.display.set_mode(size=(WIDTH, HEIGHT),

58efa04f9ab6b48e1584c6dfe78b96a930509c2f
Jericho-NSK <panchenkodv@ya.ru>
2023-03-07 18:39:02 +0700

    added docstrings

--- 2023-02.2_Flappy_Pygame_Pet_Project/window.py
+++ 2023-02.2_Flappy_Pygame_Pet_Project/window.py
@@ -6,11 +6,12 @@ from images_and_sounds import Images
 
 
 class Window:
+    """Class for creating main window and it's updating"""
     window = WINDOW
 
     def __init__(self, game):
         self.bg_rect = Images.bg.get_rect()
-        self.elements = Elements(game, self)
+        self.elements = Elements(game)
 
     def update_window(self, game):
         self.window.blit(Images.bg, (self.bg_rect.x, 0))
@@ -20,20 +21,25 @@ class Window:
 
         game.walls.draw(self.window)
 
-        self.window.blits((
-            (self.elements.score, (20, 20)),
-            (self.elements.speed, (20, 60)),
-            *((Images.heart, (WIDTH - self.elements.heart_rect.w * (heart + 1),
-                              self.elements.heart_rect.y)) for heart in range(game.lives)),
-            (game.bird.image, (game.bird.rect.centerx, game.bird.rect.centery)),
-        ), False)
+        self.window.blits(
+            (
+                (self.elements.score, (20, 20)),
+                (self.elements.speed, (20, 60)),
+                *((Images.heart, (WIDTH - self.elements.heart_rect.w * (heart + 1),
+                                  self.elements.heart_rect.y)) for heart in range(game.lives)),
+                (game.bird.image, (game.bird.rect.centerx, game.bird.rect.centery)),
+            ),
+            False,
+        )
 
         if self.elements.escape_timer:
             self.window.blit(self.elements.escape_up, self.elements.escape_up_rect.topleft)
             self.window.blit(self.elements.escape_down, self.elements.escape_down_rect.topleft)
             self.elements.escape_timer -= 1
+
         if game.bird.image != Images.bird_images[-1]:
             game.bird.flying(game)
         if game.game_starts:
             pygame.display.update()
+
         CLOCK.tick(FPS)

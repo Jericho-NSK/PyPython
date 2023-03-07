12b64e15874b4d299a1222a1e24c696b48e5fa7a
Jericho-NSK <panchenkodv@ya.ru>
2023-03-07 18:39:12 +0700

    added docstrings

--- 2023-02.2_Flappy_Pygame_Pet_Project/elements.py
+++ 2023-02.2_Flappy_Pygame_Pet_Project/elements.py
@@ -4,17 +4,18 @@ from walls import Wall
 
 
 class Elements:
+    """Class for creating elements of main window"""
 
-    def __init__(self, game, window):
+    def __init__(self, game):
         self.score = FONT.render(f'SCORE: {game.score}', True, 'red')
         self.speed = FONT.render(f'SPEED: {Wall.speed}', True, 'red')
 
         self.escape_up = FONT.render('NO PAUSES! ONLY HARDCORE!', True, 'red')
         self.escape_down = FONT.render('PRESS ESCAPE AGAIN TO GIVE UP', True, 'red')
         self.escape_up_rect = self.escape_up.get_rect()
-        self.escape_up_rect.center = WIDTH //2, HEIGHT - 3 * self.escape_up_rect.h
+        self.escape_up_rect.center = WIDTH // 2, HEIGHT - 3 * self.escape_up_rect.h
         self.escape_down_rect = self.escape_down.get_rect()
-        self.escape_down_rect.center = WIDTH //2, HEIGHT - 2 * self.escape_down_rect.h
+        self.escape_down_rect.center = WIDTH // 2, HEIGHT - 2 * self.escape_down_rect.h
         self.escape_timer = 0
 
         self.heart_rect = Images.heart.get_rect()
